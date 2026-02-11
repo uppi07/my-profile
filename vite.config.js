c77af1d30d79546d9cdd927f1bf9bc7e7da923cd
Upendra Dommaraju <80142898+uppi07@users.noreply.github.com>
2026-02-11 22:08:04 +0530

    Update API proxy URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:4000"
+      "/api": "https://my-profile-q8l1.onrender.com"
     }
   }
 });
